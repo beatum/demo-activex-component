ff738c8daad7513810c588d2fc671541279883c7
Happy <happy_heshuai@hotmail.com>
2021-11-18 16:49:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+###Register:
+C:\Windows\Microsoft.NET\Framework\v2.0.50727>regasm.exe /codebase <full path of dll file>
+###Unregister:
+C:\Windows\Microsoft.NET\Framework\v2.0.50727>regasm.exe  /u <full path of dll file>
---
+++ application.happy/DemoApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace application.happy
+{
+    [ProgId("App.Demo.Activex.Happy")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [Guid("F10CA919-2F40-4F6D-BA0E-FF5778539868")]
+    [ComVisible(true)]
+    public class DemoApp
+    {
+        [ComVisible(true)]
+        public String SayHappy() {
+            return "Happy";
+        }
+    }
+}
